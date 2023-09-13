a2353540e7c87f7c19741e713b682074d292d765
Peter Todorov <peter.todorov@live.com>
2023-09-13 19:33:11 +0200

    Compute loss and hence gradient in right order

--- loss_functions.py
+++ loss_functions.py
@@ -11,16 +11,19 @@ class MSE_Loss():
         self.y_true = ""
 
     def __call__(self, y_pred, y_true):
-        self.y_true = y_true
         self.input = y_pred
-        self.loss = float(np.sum((y_true - y_pred)**2))
+        self.y_true = y_true
+
+        # Important to have pred - true
+        # Otherwise gradient should be multiplied with -1
+        self.loss = float(np.sum((y_pred - y_true)**2))
         return self.loss
 
     def mse_derivative(self, x):
-        return -2 * x  # Explain this, why negative
+        return 2 * x
 
     def backward(self):
-        self.input_grad += float(np.sum(self.mse_derivative((self.y_true - self.input))))
+        self.input_grad += float(np.sum(self.mse_derivative((self.input - self.y_true))))
 
     def zero_grad(self):
         self.input_grad = 0.0
