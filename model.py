7fb83fb730da414807acf529d9f33a4e5e65e0dd
Peter Todorov <peter.todorov@live.com>
2023-09-13 19:33:11 +0200

    Simpler tests

--- model.py
+++ model.py
@@ -1,13 +1,11 @@
 from base_model import Base_Model
-from neuron import Neuron
 from layer import Layer
 
 
 class Model(Base_Model):
     def __init__(self, number_of_inputs, number_of_outputs):
         self.model = [
-            Layer(number_of_inputs, 2),
-            Layer(2, number_of_outputs),
+            Layer(number_of_inputs, number_of_outputs),
         ]
 
     def forward(self, x):
--- train.py
+++ train.py
@@ -8,19 +8,19 @@ from optimizers import Gradient_Decent
 
 def main():
     input_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    ground_truth = np.array([[0], [1], [1], [0]])
+    ground_truth = np.array([[0], [0], [0], [0]])
     #input_data = np.array([[0, 0], [1, 1], [2, 2], [3, 3]])
     #ground_truth = np.array([[0], [0.2], [0.4], [0.6]])
 
     loss_function = MSE_Loss()
     model = Model(len(input_data[0]), len(ground_truth[0]))
-    learning_rate = 0.01
+    learning_rate = 0.001
     optimizer = Gradient_Decent(learning_rate, model)
 
     losses = []
 
     # Train
-    epochs = 200
+    epochs = 50
     for epoch in range(epochs):
         for input, gt in zip(input_data, ground_truth):
             prediction = model(input)
@@ -30,7 +30,9 @@ def main():
             model.backward(loss_function)
             optimizer.step()
     print()
-    print(prediction, ground_truth)
+    print("Predictions")
+    for input, gt in zip(input_data, ground_truth):
+        print(model(input), gt)
 
     plt.plot(losses)
     plt.show()
