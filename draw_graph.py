1a657b1675379dba0a199c6a726d04d0311d5105
Peter Todorov <peter.todorov@live.com>
2023-09-13 19:33:11 +0200

    Draw graph right; weights as edges and outputs as nodes

--- draw_graph.py
+++ draw_graph.py
@@ -3,21 +3,52 @@ from model import Model
 from loss_functions import MSE_Loss
 
 
-def main():
-    model = Model(3, 1)
-    loss_function = MSE_Loss()
-
+def draw_graph(model, loss_function):
     dot = graphviz.Digraph(comment="ML model graph", filename="graph.gv")
     layers = model.model
+
     for i, layer in enumerate(layers):
         for j, neuron in enumerate(layer.neurons):
-            dot.node(neuron.name, neuron.name)
-            if i == len(layers) - 1:
-                dot.edge(neuron.name, loss_function.name)
-            else:
-                for next_layer_neuron in layers[i+1].neurons:
-                    dot.edge(neuron.name, next_layer_neuron.name)
+
+            # Add input nodes
+            if i == 0 and j == 0:
+                for k, input in enumerate(neuron.input):
+                    previous_layer_node_name = f"layer_{i}_out_{k}"
+                    dot.node(
+                        previous_layer_node_name,
+                        "input {:.2}\n".format(input)
+                    )
+
+            # Add single layer node
+            current_node_name = f"layer_{i+1}_out_{j}"
+            dot.node(
+                current_node_name,
+                "value {:.2}\ngrad {:.2}".format(neuron.input[j], neuron.grad)
+            )
+
+            # Add edges from previous layer to current
+            for k, weight in enumerate(neuron.weights.data):
+                previous_layer_node_name = f"layer_{i}_out_{k}"
+                dot.edge(
+                    previous_layer_node_name,
+                    current_node_name,
+                    label="w {:.2}\ng {:.2}".format(weight, neuron.weights.grad[k])
+                )
+
+    # Add loss
+    dot.node(
+        "loss",
+        "loss {:.2}\ngrad {:.2}".format(loss_function.loss, loss_function.grad)
+    )
+    dot.edge(
+        current_node_name,
+        "loss",
+        label=""
+    )
     dot.view()
 
 
-main()
+if __name__ == "__main__":
+    model = Model(2, 1)
+    loss_function = MSE_Loss()
+    draw_graph(model, loss_function)
