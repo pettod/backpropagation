7532a6a12ce69f060f54ea5ca7585bae9ad0155c
Peter Todorov <peter.todorov@live.com>
2023-09-13 19:33:11 +0200

    Use rectangle nodes for weights

--- draw_graph.py
+++ draw_graph.py
@@ -18,7 +18,7 @@ def draw_graph(model, loss_function):
                         previous_layer_node_name,
                         "input {:.2}\n".format(input),
                         style="filled",
-                        color="#FDFFA5",
+                        fillcolor="#FDFFA5",
                     )
 
             # Add single layer node
@@ -27,24 +27,38 @@ def draw_graph(model, loss_function):
                 current_node_name,
                 "value {:.2}\ngrad {:.2}".format(neuron.input[j], neuron.grad),
                 style="filled",
-                color="#FF4444",
+                fillcolor="#FF4444",
             )
 
             # Add edges from previous layer to current
             for k, weight in enumerate(neuron.weights.data):
                 previous_layer_node_name = f"layer_{i}_out_{k}"
-                dot.edge(
-                    previous_layer_node_name,
-                    current_node_name,
-                    label="w {:.2}\ng {:.2}".format(weight, neuron.weights.grad[k])
-                )
+                if True:
+                    weight_node_name = f"{previous_layer_node_name}_{current_node_name}"
+                    dot.node(
+                        weight_node_name,
+                        "weight {:.2}\ngrad {:.2}".format(weight, neuron.weights.grad[k]),
+                        shape="box",
+                        style="filled",
+                        fillcolor="#64FF73",
+                        fontsize="10pt",
+                    )
+                    dot.edge(previous_layer_node_name, weight_node_name)
+                    dot.edge(weight_node_name, current_node_name)
+                else:
+                    dot.edge(
+                        previous_layer_node_name,
+                        current_node_name,
+                        label="w {:.2}\ng {:.2}".format(weight, neuron.weights.grad[k]),
+                        fontsize="10pt",
+                    )
 
     # Add loss
     dot.node(
         "loss",
         "loss {:.2}\ngrad {:.2}".format(loss_function.loss, loss_function.grad),
                         style="filled",
-        color="#448BFF",
+        fillcolor="#448BFF",
     )
     dot.edge(
         current_node_name,
