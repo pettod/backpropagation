692f5aae9c6aec2b99c4fa7833c2e94f822889df
Peter Todorov <peter.todorov@live.com>
2023-09-17 15:47:41 +0200

    Implement Softmax activation function

--- activations.py
+++ activations.py
@@ -1,3 +1,27 @@
+import math
+
+
+class BaseActivation():
+    def inputMax(self, input):
+        input_max = None
+        for value in input:
+            if input_max is None:
+                input_max = value
+            elif value > input_max:
+                input_max = value
+        return input_max
+
+    def inputSum(self, input):
+        input_sum = 0.0
+        for value in input:
+            input_sum += value
+        return input_sum
+
+    def backward(self):
+        for neuron in self.neurons:
+            neuron.input.grad += self.derivative(neuron)
+
+
 class Activation():
     def __init__(self):
         self.input = ""
@@ -8,7 +32,7 @@ class Activation():
         self.grad = 0.0
 
 
-class ReLU():
+class ReLU(BaseActivation):
     def __init__(self, inputs):
         self.neurons = [Activation() for i in range(inputs)]
 
@@ -18,7 +42,26 @@ class ReLU():
             neuron.data = max(0, value.data)
         return self.neurons
 
-    def backward(self):
-        for neuron in self.neurons:
-            if neuron.data > 0:
-                neuron.input.grad += neuron.grad
+    def derivative(self, neuron):
+        return neuron.grad if neuron.data > 0.0 else 0.0
+
+
+class Softmax(BaseActivation):
+    def __init__(self, inputs):
+        self.neurons = [Activation() for i in range(inputs)]
+        self.outputs = []
+
+    def __call__(self, input):
+        input_max = self.inputMax(input)
+        input_sum = self.inputSum(input)
+
+        # Subtracting the maximum value for numerical stability
+        for neuron, value in zip(self.neurons, input):
+            neuron.input = value
+            softmax_value = math.exp(value - input_max) / input_sum
+            neuron.data = softmax_value
+            self.outputs.append(softmax_value)
+        return self.neurons
+
+    def derivative(self, neuron):
+        return neuron.data * (1.0 - neuron.data)
