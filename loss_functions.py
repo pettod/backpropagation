dde0fdc8fda4a39f60a45ab3434ae900ae7a2648
Peter Todorov <peter.todorov@live.com>
2023-09-17 13:02:11 +0200

    Implement CrossentropyLoss

--- loss_functions.py
+++ loss_functions.py
@@ -1,10 +1,32 @@
-class MSE():
+import math
+
+
+class BaseLoss():
     def __init__(self):
         self.loss = 0.0
         self.grad = 1.0
         self.input = 0.0
-        self.name = "loss_MSE"
         self.y_true = ""
+
+    def backward(self):
+        input_grad = []
+
+        # Compute loss grad with respect to the inputs
+        for pred, true in zip(self.input, self.y_true):
+            input_grad.append(float(self.derivative(pred.data, true)))
+
+        # Set the inputs grad
+        for i in range(len(self.input)):
+            self.input[i].grad += input_grad[i]
+
+    def derivative(self, pred, true):
+        pass
+
+
+class MSE(BaseLoss):
+    def __init__(self):
+        self.name = "MSE"
+        super().__init__()
 
     def __call__(self, y_pred, y_true):
         self.input = y_pred
@@ -17,16 +39,33 @@ class MSE():
             self.loss += float((pred.data - true)**2)
         return self.loss
 
-    def derivative(self, x):
-        return 2 * x
+    def derivative(self, pred, true):
+        return 2 * (pred - true)
 
-    def backward(self):
-        input_grad = 0.0
 
-        # Compute loss grad with respect to the inputs
-        for pred, true in zip(self.input, self.y_true):
-            input_grad += float(self.derivative((pred.data - true)))
+class CrossEntropyLoss(BaseLoss):
+    def __init__(self):
+        self.name = "CrossEntropyLoss"
+        self.epsilon = 1e-15  # Small constant to avoid division by zero
+        super().__init__()
 
-        # Set the inputs grad
-        for pred in self.input:
-            pred.grad += input_grad
+    def __call__(self, y_pred, y_true):
+        # Clip values between (epsilon, 1 - epsilon)
+        for i in range(len(y_pred)):
+            y_pred[i] = max(min(1 - self.epsilon, y_pred[i]), self.epsilon)
+
+        self.input = y_pred
+        self.y_true = y_true
+
+        # Loss
+        self.loss = 0.0
+        for i in range(len(y_pred)):
+            a = y_true[i] * math.log(float(y_pred[i].data))
+            b = (1 - y_true[i]) * math.log(1 - float(y_pred[i].data))
+            self.loss += (a + b)
+        self.loss *= -1
+
+        return self.loss
+
+    def derivative(self, pred, true):
+        return -(true / pred - (1 - true) / (1 - pred))
